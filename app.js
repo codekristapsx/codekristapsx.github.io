b461fdfe1e0e0373d0cec6d335dab2bdbb217a4f
Kristaps Kozlovskis <218506029+codekristapsx@users.noreply.github.com>
2025-06-30 21:14:21 +0300

    Added code

---
+++ app.js
@@ -0,0 +1,7 @@
+// Green button to GitHub
+
+const button = document.querySelector('.green_button');
+
+button.addEventListener('click', function() {
+    window.open('https://github.com/codekristapsx', '_blank');
+})
